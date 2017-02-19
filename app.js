9340bccd88e91accded244807c132bd49e341b90
Andrew Mayer <andrewmayer515@gmail.com>
2017-02-19 11:04:07 -0600

    refactor configs

--- app.js
+++ app.js
@@ -1,35 +1,28 @@
-var irc = require("tmi.js");
-var auth = require("./authentication.json")
+var twitch = require("tmi.js");
 var moment = require("moment");
+var auth = require("./data/authentication.json");
+var options = require("./data/options.json");
+var helpers = require("./helpers.js");
 
-//Get the time that the node service starts
-var startTime = moment();
-
-//Set configs for Twitch API
-var options = {
-	options: {
-		debug: false
-	},
-	connection: {
-        cluster: "aws",
-        reconnect: true
-    },
-    identity: {
-        username: auth.username,
-        password: auth.password
-    },
-    channels: ["#andrewmayer515"]
-}
-
-var client = new irc.client(options);
+//set the channel to talk in
 var channelName = "andrewmayer515";
+//pass in bot config
+options.identity.username = auth.username;
+options.identity.password = auth.password;
 
+var client = new twitch.client(options);
 client.connect();
+console.log("Client connected");
+
+var textResponse = function(response){
+	client.action(channelName, response)
+};
 
 client.on("chat", function (channel, user, message, self) {
 	switch (message) {
 		case "hi":
-			client.action(channelName, "Hello " + user.username);
+			var text = "Hello " + user.username;
+			textResponse(text);
 			break;
 		case "*time":
 			client.action(channelName, "Andrew has been online for x minutes");
@@ -37,7 +30,4 @@ client.on("chat", function (channel, user, message, self) {
 		default:
 			break;
 	}
-	/**if (message === "hi") {
-		client.action(channelName, "Hello " + user.username);
-	}**/
 });
